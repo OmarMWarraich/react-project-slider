73faee51feeee6f05784dd1f26aa2c0bdafbccf6
OmarMWarraich <earthling986@gmail.com>
2021-04-29 08:35:57 +0500

    first commit

---
+++ src/Data.js
@@ -0,0 +1,58 @@
+const Reviews = [
+    {
+        id: 1,
+        image: 'https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cGVyc29ufGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+        name: 'Andrew Symonds',
+        title: 'web developer',
+        quote: "Lorem ipsum, dolor sit amet consectetur adipisicing elit."
+        
+    },
+    {
+        id: 2,
+        image: 'https://images.unsplash.com/photo-1542103749-8ef59b94f47e?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGVyc29ufGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+        name: 'Milla Jovovich',
+        title: 'web designer',
+        quote: "Lorem ipsum, dolor sit amet consectetur adipisicing elit."
+        
+    },
+    {
+        id: 3,
+        image: 'https://images.unsplash.com/photo-1547425260-76bcadfb4f2c?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cGVyc29ufGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+        name: 'Sikandar Khan',
+        title: 'Backend developer',
+        quote: "Lorem ipsum, dolor sit amet consectetur adipisicing elit."
+        
+    },
+    {
+        id: 4,
+        image: 'https://images.unsplash.com/photo-1500048993953-d23a436266cf?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fHBlcnNvbnxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+        name: 'Marc Anthony',
+        title: 'Data Scientist',        
+        quote: "Lorem ipsum, dolor sit amet consectetur adipisicing elit."
+        
+    },
+    {
+        id: 5,
+        image: 'https://images.unsplash.com/photo-1573140247632-f8fd74997d5c?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fHBlcnNvbnxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+        name: 'Stephany Johanes',
+        title: 'intern',
+        quote: "Lorem ipsum, dolor sit amet consectetur adipisicing elit."
+        
+    },
+    {
+        id: 6,
+        image: 'https://images.unsplash.com/photo-1578489758854-f134a358f08b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzV8fHBlcnNvbnxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+        name: 'Elizabeth Shue',
+        title: 'AI Specialist',
+        quote: "Lorem ipsum, dolor sit amet consectetur adipisicing elit."
+        
+    }, {
+        id: 7,
+        image: 'https://images.unsplash.com/photo-1593219191991-47517a4bb8d5?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Njd8fHBlcnNvbnxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+        name: 'peter aandre',
+        title: 'the boss',
+        quote: "Lorem ipsum, dolor sit amet consectetur adipisicing elit."
+        
+    },
+]
+export default Reviews;
